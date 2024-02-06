75aa28c68057b7dd26cfc915c46580fb854a3b35
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2024-02-06 13:16:51 +0100

    Finished loader and added counter-data

--- xmlloader.py
+++ xmlloader.py
@@ -1,21 +1,26 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-def load_xml():
-    tree = ET.parse('data/xmldata.xml')
-    root = tree.getroot()
+class XmlLoader:
 
-    # Extract data from XML and create a list of dictionaries
-    data_list = []
-    for record in root.findall('.//record'):
-        data_dict = {}
-        for field in record.findall('.//field'):
-            data_dict[field.get('name')] = field.text if field.text else None
-        data_list.append(data_dict)
+    @staticmethod
+    def load_counter():
+        tree = ET.parse('data/xmlcounterdata.xml')
+        root = tree.getroot()
 
-    # Create a DataFrame
-    df = pd.DataFrame(data_list)
+        # Extract data from XML and create a list of dictionaries
+        data_list = []
+        for record in root.findall('.//record'):
+            data_dict = {}
+            for field in record.findall('.//field'):
+                data_dict[field.get('name')] = field.text if field.text else None
+            data_list.append(data_dict)
 
-    # Pivot the DataFrame
-    df_pivot = df.pivot(index='Year', columns=['Country or Area', 'Item'], values='Value').reset_index()
-    return df
+        # Create a DataFrame
+        df = pd.DataFrame(data_list)
+
+        # Pivot the DataFrame
+        df_pivot = df.pivot(index='Year', columns=['Country or Area', 'Item'], values='Value').reset_index()
+
+        # Display the resulting DataFrame
+        return df
